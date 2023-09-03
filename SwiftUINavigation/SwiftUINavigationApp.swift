610834a7eaf82ef939306d3fc685f352d5e31306
Klaudius ivan <klaudiusivanr>
2023-09-03 18:40:38 +0700

    Initial Commit

---
+++ SwiftUINavigation/SwiftUINavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationApp.swift
+//  SwiftUINavigation
+//
+//  Created by Klaudius Ivan Anteraja on 03/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
